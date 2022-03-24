ff619861a42ab5bb2dd5f8b4b42ce5589bf11c4d
Skipper047 <Ski_pper@bk.ru>
2022-03-24 18:43:09 +0600

    setCommit

--- lesson_8_1/src/kg/geektech/game/players/Warrior.java
+++ lesson_8_1/src/kg/geektech/game/players/Warrior.java
@@ -3,6 +3,7 @@ package kg.geektech.game.players;
 import kg.geektech.game.general.RPG_Game;
 
 public class Warrior extends Hero {
+
     public Warrior(String name, int health, int damage) {
         super(name, health, damage, SuperAbility.CRITICAL_DAMAGE);
     }
